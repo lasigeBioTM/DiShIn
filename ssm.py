9cbf23a46410c9677f14508cae6a0f59d8e19a6f
Francisco M Couto <franciscomcouto@gmail.com>
2017-06-06 17:32:07 +0100

    Add files via upload

--- ssm.py
+++ ssm.py
@@ -1,6 +1,9 @@
 import sqlite3
 import math
 
+intrinsic = False
+mica = False
+
 connection = sqlite3.connect('semanticbase.db')
 
 def descendents (entry):
@@ -17,6 +20,21 @@ def descendents (entry):
        descendents.append(row[0])
 
     return descendents
+
+def get_id (name):
+
+    iden = 0
+    
+    rows = connection.execute('''
+        SELECT id
+        FROM entry
+        WHERE name = ?
+    ''', (name,))
+
+    for row in rows:
+       iden = row[0]
+
+    return iden
 
 def num_entries ():
 
@@ -76,9 +94,38 @@ def information_content_intrinsic (entry):
     return -math.log(freq/maxfreq)
 
 def information_content (entry):
-    #return information_content_intrinsic (entry)
-    return information_content_extrinsic (entry)
+    if intrinsic:
+        return information_content_intrinsic (entry)
+    else: 
+        return information_content_extrinsic (entry)
 
+def num_paths (entry1, ancestor):
+    
+    rows = connection.execute('''
+        SELECT npaths
+        FROM num_paths p
+        WHERE p.entry1=? AND p.entry2=? 
+        ''', (entry1, ancestor, ))
+
+    for row in rows:
+        npaths = row[0] + 1.0 
+
+    return npaths
+
+def shared_ic_dca (entry1, entry2):
+
+    ancestors = common_ancestors(entry1, entry2)
+    dca = {}
+
+    for anc in ancestors:        
+        pd = abs(num_paths(entry1, anc) - num_paths(entry2, anc))
+        ic = information_content(anc)
+        dca[pd] = max(information_content(anc),dca.get(pd,0));
+        
+    values = dca.values()
+
+    return (float(sum(values)) / float(len(values)))
+    
 def shared_ic_mica (entry1, entry2):
 
     ic = 0 
@@ -90,9 +137,12 @@ def shared_ic_mica (entry1, entry2):
 
     return ic
 
+
 def shared_ic (entry1, entry2):
-    #return shared_ic_dca (entry1, entry2)
-    return shared_ic_mica (entry1, entry2)
+    if mica:
+        return shared_ic_mica (entry1, entry2)
+    else:
+        return shared_ic_dca (entry1, entry2)
 
 def ssm_resnik (entry1, entry2):
 
@@ -102,10 +152,24 @@ def ssm_lin (entry1, entry2):
 
     return 2*shared_ic(entry1, entry2) / (information_content(entry1) + information_content(entry2))
 
+#################################
+# Examples
+#################################
 
-print ssm_resnik (6, 7)
+intrinsic = False
 
-print ssm_lin (6, 7)
+mica = False
 
+print ssm_resnik (get_id('palatium'), get_id('gold'))
 
+print (-math.log(6.0/9.0)-math.log(9.0/9.0))/2
 
+mica = True
+
+print ssm_resnik (get_id('palatium'), get_id('gold'))
+
+print -math.log(6.0/9.0)
+
+print ssm_lin (get_id('palatium'), get_id('gold'))
+
+print (2*-math.log(6.0/9.0))/(-math.log(2.0/9.0)-math.log(2.0/9.0))
