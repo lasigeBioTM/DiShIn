c865e55aa2a9d8058bcdcc493f6822deabf4b181
Francisco Couto <franciscomcouto@gmail.com>
2017-07-03 16:10:47 +0100

    print update

--- metals.py
+++ metals.py
@@ -3,7 +3,7 @@ import math
 import ssm
 import semanticbase
 
-semanticbase.create('metals.owl', 'metals.db', 'https://raw.githubusercontent.com/lasigeBioTM/ssm/master/metals.owl#')
+semanticbase.create('metals.owl', 'metals.db', 'https://raw.githubusercontent.com/lasigeBioTM/ssm/master/metals.owl#', 'http://www.w3.org/2000/01/rdf-schema#subClassOf')
 
 ssm.semantic_base('metals.db')
 
@@ -14,37 +14,36 @@ ssm.mica = False
 platinum = ssm.get_id('platinum')
 
 gold = ssm.get_id('gold')
-hp
+
 silver = ssm.get_id('silver')
 
 copper = ssm.get_id('copper')
 
 palladium = ssm.get_id('palladium')
 
-print ssm.ssm_resnik (platinum, gold)
-
-print (-math.log(6.0/9.0)-math.log(9.0/9.0))/2
+print ('resnik dishin extrinsic similarity platinum gold = ' + str(ssm.ssm_resnik (platinum, gold)))
+print ('                            mathematical formula = ' + str((-math.log(6.0/9.0)-math.log(9.0/9.0))/2))
 
 ssm.mica = True
 
-print ssm.ssm_resnik (platinum, gold)
+print ('  resnik mica extrinsic similarity platinum gold = ' + str(ssm.ssm_resnik (platinum, gold)))
+print ('                            mathematical formula = ' + str(-math.log(6.0/9.0)))
 
-print -math.log(6.0/9.0)
+print ('     lin mica extrinsic similarity platinum gold = ' + str(ssm.ssm_lin (platinum, gold)))
+print ('                            mathematical formula = ' + str((2*-math.log(6.0/9.0))/(-math.log(2.0/9.0)-math.log(2.0/9.0))))
 
-print ssm.ssm_lin (platinum, gold)
 
-print (2*-math.log(6.0/9.0))/(-math.log(2.0/9.0)-math.log(2.0/9.0))
-
-print ssm.ssm_resnik (platinum, copper)
-print ssm.ssm_resnik (platinum, gold)
-print ssm.ssm_resnik (copper, gold)
-print ssm.ssm_resnik (platinum, palladium)
-print ssm.ssm_resnik (silver, gold)
+print ('resnik **mica** extrinsic similarity:')
+print ('   platinum copper    = ' + str(ssm.ssm_resnik (platinum, copper)))
+print ('   platinum gold      = ' + str(ssm.ssm_resnik (platinum, gold)))
+print ('   copper gold        = ' + str(ssm.ssm_resnik (copper, gold)))
+print ('   platinum palladium = ' + str(ssm.ssm_resnik (platinum, palladium)))
+print ('   silver gold        = ' + str(ssm.ssm_resnik (silver, gold)))
 
 ssm.mica = False
-
-print ssm.ssm_resnik (platinum, copper)
-print ssm.ssm_resnik (platinum, gold)
-print ssm.ssm_resnik (copper, gold)
-print ssm.ssm_resnik (platinum, palladium)
-print ssm.ssm_resnik (silver, gold)
+print ('resnik **dishin** extrinsic similarity:')
+print ('   platinum copper    = ' + str(ssm.ssm_resnik (platinum, copper)))
+print ('   platinum gold      = ' + str(ssm.ssm_resnik (platinum, gold)))
+print ('   copper gold        = ' + str(ssm.ssm_resnik (copper, gold)))
+print ('   platinum palladium = ' + str(ssm.ssm_resnik (platinum, palladium)))
+print ('   silver gold        = ' + str(ssm.ssm_resnik (silver, gold)))
